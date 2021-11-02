b2f16ddcc5268ebc1703bcb96acfe919ffac8de9
Vijay <vks002@yahoo.com>
2021-11-02 17:05:08 +0530

    changed names of defines to be inline with home assistant secrets file

--- include/secrets_example.h
+++ include/secrets_example.h
@@ -7,16 +7,19 @@
 #ifndef SECRETS_H
 #define SECRETS_H
 
-#define SSID1 "YOUR SSID"
-#define SSID1_PSWD "YOUR PSWD"
-#define SSID2 "YOUR SSID2"
-#define SSID2_PSWD "YOUR PSWD2"
-#define OTA_PSWD "API PSWD"
-#define MQTT_SERVER1 IPAddress(192,XXX,X,XX)
-#define MQTT_PORT1 1883
-#define MQTT_USER1 "MQTT USR"
-#define MQTT_PSWD1 "MQTT PSWD"
-#define GATEWAY1 IPAddress(192,168,1,1)
-#define SUBNET1 IPAddress(255,255,255,0)
+#define primary_ssid "YOUR SSID"
+#define primary_ssid_pswd "YOUR PSWD"
+#define gf_ssid "YOUR SSID2"
+#define gf_ssid_pswd "YOUR PSWD2"
+#define default_gateway IPAddress(192,168,1,1)
+#define default_dns IPAddress(192,168,1,1)
+#define subnet_mask IPAddress(255,255,255,0)
+#define mqtt_broker IPAddress(192,168,1,XXX)
+#define mqtt_port 1883
+#define mqtt_uname "XXX"
+#define mqtt_pswd "XXXX"
+#define ota_pswd "XXXX"
+#define api_pswd "XXXX"
+#define captive_portal_pswd "XXXX"
 
 #endif
