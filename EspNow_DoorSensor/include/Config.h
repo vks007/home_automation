d06fddbaf18ea051709df48d83f01bc4e6eb83a0
Vijay <vks002@yahoo.com>
2022-01-29 23:14:37 +0530

    working version with RTC memory for wifi channel

--- EspNow_DoorSensor/include/Config.h
+++ EspNow_DoorSensor/include/Config.h
@@ -3,11 +3,12 @@
 
 // you cant use strings so you have to use #defines for numeric values
 // Ref :https://stackoverflow.com/questions/2335888/how-to-compare-strings-in-c-conditional-preprocessor-directives
-#define USE_MAIN_DOOR 1
-#define USE_TERRACE_DOOR 2
-#define USE_BALCONY_DOOR 3
+//The following defines the device type for which the program is being compiled, this is passed as a compile time parameter in platform.ini
+#define MAIN_DOOR 1
+#define TERRACE_DOOR 2
+#define BALCONY_DOOR 3
 
-#if (DEVICE == USE_MAIN_DOOR) 
+#if (DEVICE == MAIN_DOOR) 
   #pragma message "Compiling the program for the device: MAIN_DOOR"
   #define DEVICE_NAME             "main_door"
   //Hold pin will hold CH_PD HIGH till we're executing the setup, the last step would be set it LOW which will power down the ESP
@@ -15,9 +16,12 @@
   #define SIGNAL_PIN0 1 //Bit 1 of the signal which indicates the message type
   #define SIGNAL_PIN1 3 //Bit 2 of the signal which indicates the message type
   //State Mapping of SIGNAL_PIN0 SIGNAL_PIN1:: 11=>IDLE , 00=> SENSOR_WAKEUP , 01=> SENSOR OPEN , 10=> SENSOR CLOSED
+  #define MY_ROLE         ESP_NOW_ROLE_COMBO              // set the role of this device: CONTROLLER, SLAVE, COMBO
+  #define RECEIVER_ROLE   ESP_NOW_ROLE_COMBO              // set the role of the receiver
   uint8_t gatewayAddress[] = GATEWAY_GF_MAC; //comes from secrets.h
+  constexpr char WIFI_SSID[] = gf_ssid;// from secrets.h
 
-#elif (DEVICE == USE_TERRACE_DOOR)
+#elif (DEVICE == TERRACE_DOOR)
   #pragma message "Compiling the program for the device: TERRACE_DOOR"
   #define DEVICE_NAME             "terrace_door" // This becomes the postfix of the final MQTT topic under which messages are published
   //Hold pin will hold CH_PD HIGH till we're executing the setup, the last step would be set it LOW which will power down the ESP
@@ -25,15 +29,21 @@
   #define SIGNAL_PIN0 1 //Bit 1 of the signal which indicates the message type
   #define SIGNAL_PIN1 3 //Bit 2 of the signal which indicates the message type
   //State Mapping of SIGNAL_PIN0 SIGNAL_PIN1:: 11=>IDLE , 00=> SENSOR_WAKEUP , 01=> SENSOR OPEN , 10=> SENSOR CLOSED
+  #define MY_ROLE         ESP_NOW_ROLE_COMBO              // set the role of this device: CONTROLLER, SLAVE, COMBO
+  #define RECEIVER_ROLE   ESP_NOW_ROLE_COMBO              // set the role of the receiver
   uint8_t gatewayAddress[] = GATEWAY_FF_MAC; //comes from secrets.h
+  constexpr char WIFI_SSID[] = primary_ssid;// from secrets.h
 
-#elif (DEVICE == USE_BALCONY_DOOR)
+#elif (DEVICE == BALCONY_DOOR)
   #pragma message "Compiling the program for the device: BALCONY_DOOR"
   #define DEVICE_NAME             "balcony_door"
   #define HOLD_PIN 0  // defines GPIO0 as the hold pin (will hold CH_PD high untill we power down).
   #define SIGNAL_PIN0 5 //Bit 1 of the signal which indicates the message type
   #define SIGNAL_PIN1 4 //Bit 2 of the signal which indicates the message type
+  #define MY_ROLE         ESP_NOW_ROLE_COMBO              // set the role of this device: CONTROLLER, SLAVE, COMBO
+  #define RECEIVER_ROLE   ESP_NOW_ROLE_COMBO              // set the role of the receiver
   uint8_t gatewayAddress[] = GATEWAY_FF_MAC; //comes from secrets.h
+  constexpr char WIFI_SSID[] = primary_ssid;// from secrets.h
 
 #else
   #error "Device type not selected, see Door_config.h"
--- EspNow_DoorSensor/src/main.cpp
+++ EspNow_DoorSensor/src/main.cpp
@@ -4,7 +4,7 @@
  * This ESP when it receives power, holds it power by setting CH_HOLD pin to HIGH 
  * It then reads the sensor value of the door contact and sends that to a ESP gateway via espnow
  * It then powers itself off by pulling down the CH_HOLD pin. 
- * If TESTING_MODE is defined then we can use this ESP without the ATTINY to test the setup. In this mode it does not power itself OFF. 
+ * If TESTING_MODE is ON then we can use this ESP without the ATTINY to test the setup. In this mode it does not power itself OFF and keeps toggling the sensor state
  * In testing mode, it keeps toggling the door sensor contact ON and OFF. It also repurposes the Rx & Tx pin for Serial instead of GPIO pins for ESP01
  * TO DO :
  * - store the last used channel in the RTC memory , it currently spends ~2 sec in obtaining the channel
@@ -21,6 +21,7 @@
 #pragma message STR(DEVICE)
 #endif
 */
+#define DEBUG //BEAWARE that this statement should be before #include "Debugutils.h" else the macros wont work as they are based on this #define
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
@@ -28,15 +29,14 @@
 #include "secrets.h"
 #include "espnowMessage.h" // for struct of espnow message
 #include "Config.h"
-#include<time.h>
+#include <time.h>
+#include "myutils.h" //include utility functions like read/write RTC mem
+#include "espnowUtil.h" //defines all util functions for espnow functionality
 
 // This is how you assign a numeric value to a #define constant
-#define TESTING_MODE (0)
+#define TESTING_MODE (0) //can be either 0 or 1
 
-//#define DEBUG //BEAWARE that this statement should be before #include "Debugutils.h" else the macros wont work as they are based on this #define
 #include "Debugutils.h" //This file is located in the Sketches\libraries\DebugUtils folder
-#define WAIT_TIMEOUT 50 // time in millis to wait for acknowledgement of the message sent
-#define MAX_MESSAGE_RETRIES 4 // No of times a message is retries to be sent before dropping the message
 #define SLEEP_TIME 60e6 // sleep time interval in microseconds
 
 #define VERSION "2.2"
@@ -46,7 +46,6 @@ const char compile_version[] = VERSION " " __DATE__ " " __TIME__; //note, the 3 
 #define SENSOR_WAKEUP 1
 #define SENSOR_OPEN 2
 #define SENSOR_CLOSED 3
-#define MAX_MQTT_CONNECT_RETRY 4 //max no of retries to connect to MQTT server
 
 #define MSG_ON 1 //payload for ON
 #define MSG_OFF 0//payload for OFF
@@ -58,49 +57,25 @@ const char compile_version[] = VERSION " " __DATE__ " " __TIME__; //note, the 3 
 
 ADC_MODE(ADC_VCC);//connects the internal ADC to VCC pin and enables measuring Vcc
 
-
-#define MY_ROLE         ESP_NOW_ROLE_COMBO              // set the role of this device: CONTROLLER, SLAVE, COMBO
-#define RECEIVER_ROLE   ESP_NOW_ROLE_COMBO              // set the role of the receiver
-
-typedef struct esp_now_peer_info {// this is defined in esp_now.h for ESP32 but not in espnow.h for ESP8266
-  u8 peer_addr[6];
-  uint8_t channel;
-  uint8_t encrypt;
-}esp_now_peer_info_t;
-
 espnow_message myData;
-constexpr char WIFI_SSID[] = primary_ssid;// from secrets.h
-volatile bool bSuccess = false;
-volatile bool bResultReady = false;
 
 // MAC Address , This should be the address of the softAP (and NOT WiFi MAC addr obtained by WiFi.macAddress()) if the Receiver uses both, WiFi & ESPNow
 // You can get the address via the command WiFi.softAPmacAddress() , usually it is one decimal no after WiFi MAC address
 
 /*
- * Gets the WiFi channel of the SSID of your router, It has to be on the same channel as this one as the receiver will listen to ESPNow messages on the same 
- * While theritically it should be possible for WiFi and ESPNow to work on different channels but it seems due to some bug (or behavior) this isnt possible with espressif
+ * Callback when data is sent , It sets the bResultReady flag to true on successful delivery of message
+ * The flag is set to false in the main loop where data is sent and then the code waits to see if it gets set to true, if not it retires to send
  */
-int32_t getWiFiChannel(const char *ssid) {
-  if (int32_t n = WiFi.scanNetworks()) {
-      for (uint8_t i=0; i<n; i++) {
-          if (!strcmp(ssid, WiFi.SSID(i).c_str())) {
-              return WiFi.channel(i);
-          }
-      }
-  }
-  return 0;
-}
-
-// callback when data is sent
 esp_now_send_cb_t OnDataSent([](uint8_t *mac_addr, uint8_t status) {
-  bSuccess = (status == 0 ? true : false);
-  DPRINT("\r\nLast Packet Send Status:\t");
+  bResult = (status == 0? true:false);
+//  bResult = status;
+  DPRINT("Last Packet Send Status:");
   DPRINTLN(status == 0 ? "Delivery Success" : "Delivery Fail");
   bResultReady = true;
 });
 
 /*
- * Callback called on sending a message.
+ * Callback called on receiving a message. Nothing to do here for now
  */
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   espnow_message msg;
@@ -109,6 +84,7 @@ void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
 };
 
 void setup() {
+  //Set the HOLD pin HIGH so that the ESP maintains power to itself. We will set it to low once we're done with the job, terminating power to ESP
   pinMode(HOLD_PIN, OUTPUT);
   digitalWrite(HOLD_PIN, HIGH);  // sets GPIO0 to high (this holds CH_PD high even if the input signal goes LOW)
   // For us to use Rx and Tx pin as inputs we have to define the pins as below else they would continue to be Serial pins
@@ -120,52 +96,20 @@ void setup() {
   }
   pinMode(SIGNAL_PIN0, INPUT_PULLUP);
   pinMode(SIGNAL_PIN1, INPUT_PULLUP);
+  #else
+    delay(1000); //introducing a delay in testing mode with DEBUG ON as the serial monitor on platformio isn't fast enough to catch messages
   #endif
   //Init Serial Monitor
   DBEGIN(115200);
   DPRINTLN("Starting up");
-  // Set device as a Wi-Fi Station and set channel
-  WiFi.mode(WIFI_STA);
 
-  int32_t channel = getWiFiChannel(WIFI_SSID);
-// Sometimes after loading the new firmware the channel doesnt change to the required one, I have to reset the board to do the same.
-// It rather reports the channel as 0 , I will have to look into this and solve, I think I saw some discussion around this where ESP remembers the last channel used 
-// I think I have to fo a WiFi.disconnect to circumvent this, will try this out.
+  Initilize_espnow();
 
-  WiFi.disconnect(); // trying to see if the issue of sometimes it not setting the right channel gets solved by this.
-  // To change the channel you have to first call wifi_promiscuous_enable(true) , change channel and then call wifi_promiscuous_enable(false)
-//  WiFi.printDiag(Serial); // Uncomment to verify channel number before
-  wifi_promiscuous_enable(true);
-  wifi_set_channel(channel);
-  wifi_promiscuous_enable(false);
-  delay(10);
-  short ch = wifi_get_channel();
-  DPRINTFLN("channel:%d",ch);
-  //strange behavior : If I define ch as byte and make the comparison below , the ESP resets due to WDT and then hangs
-  if(ch == 0)
-  {
-    DPRINTLN("WiFi Channel not set properly, restarting");
-    ESP.restart();
-  }
-//  WiFi.printDiag(Serial); // Uncomment to verify channel change after
-
-  //Init ESP-NOW
-  if (esp_now_init() != 0) {
-    DPRINTLN("Error initializing ESP-NOW");
-    return;
-  }
-
-  esp_now_set_self_role(MY_ROLE);
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
+  // register callbacks for events when data is sent and data is received
   esp_now_register_send_cb(OnDataSent);
   esp_now_register_recv_cb(OnDataRecv);
 
-  //Add peer , There is no method in ESP8266 to add a peer by passing esp_now_peer_info_t object unlike ESP32
-  if (esp_now_add_peer((u8*)gatewayAddress, RECEIVER_ROLE, channel, NULL, 0) != 0){
-    DPRINTLN("Failed to add peer");
-    return;
-  }
+  refreshPeer();
 
   // char device_id[13];
   // String wifiMacString = WiFi.macAddress();
@@ -175,63 +119,57 @@ void setup() {
   // DPRINTF("deviceid:%s\n",device_id);
 
  
-  //TO DO : you can read the input values in a single statement directly from registers and then compare using a mask
-  // TO DO : Shift the reading of pins to before reading config so that even if ATTiny removes the signal, ESP can still take its own time in publishing the message
-  //Read the type of message we've got from the ATiny
+  // The main code below loops N times if we're testing to give 10 readings, else it reads once and then kills power to itself
   #if (TESTING_MODE)
-    // Keep toggling the message type in testing mode
-    CURR_MSG = (CURR_MSG== SENSOR_OPEN?SENSOR_CLOSED:SENSOR_OPEN);
+    size_t loop_count = 3;
   #else
-  if((digitalRead(SIGNAL_PIN0) == LOW) && (digitalRead(SIGNAL_PIN1) == LOW))
-    CURR_MSG = SENSOR_WAKEUP;
-  else if((digitalRead(SIGNAL_PIN0) == LOW) && (digitalRead(SIGNAL_PIN1) == HIGH))
-    CURR_MSG = SENSOR_OPEN;
-  else if((digitalRead(SIGNAL_PIN0) == HIGH) && (digitalRead(SIGNAL_PIN1) == LOW))
-    CURR_MSG = SENSOR_CLOSED;
-  //else nothing to do, invalid mode
+    size_t loop_count = 1;
   #endif
 
-  // populate the fixed values for a message
-  strcpy(myData.device_name,DEVICE_NAME);
-  myData.intvalue1 = (CURR_MSG == SENSOR_OPEN? MSG_ON:MSG_OFF);
-  myData.intvalue2 = ESP.getVcc();
-  myData.intvalue3 = digitalRead(SIGNAL_PIN0);
-  myData.intvalue4 = digitalRead(SIGNAL_PIN1);
-  myData.floatvalue1 = 0;
-  myData.floatvalue2 = 0;
-  myData.floatvalue3 = 0;
-  myData.floatvalue4 = 0;
-  myData.chardata1[0] = '\0';
-  strncpy(myData.chardata2,compile_version,15);//only copy the first 15 chars as compile_version is longer
-  myData.chardata2[15] = '\0';//add the null character else it will result in overflow of memory
-  //generate a random value for the message id
-  srand(time(0));
-  myData.message_id = rand();
-  // try to send the message MAX_MESSAGE_RETRIES times if it fails
-  for(short i = 0;i<MAX_MESSAGE_RETRIES;i++)
+  for (size_t i = 0; i < loop_count; i++)
   {
-    int result = esp_now_send(gatewayAddress, (uint8_t *) &myData, sizeof(myData));
-    // If devicename is not given then generate one from MAC address stripping off the colon
-    long waitTimeStart = millis();
-    if (result == 0) DPRINTLN("Sent with success");
-    else DPRINTLN("Error sending the data");
-    //get a confirmation of successful delivery , else will try again
-    while(!bResultReady && ((millis() - waitTimeStart) < WAIT_TIMEOUT))
-    {
-      delay(1);
-    }
-    bResultReady = false; // prepare for next iteration
-    if(bSuccess)
-      break;
-  }
+    //TO DO : you can read the input values in a single statement directly from registers and then compare using a mask
+    // TO DO : Shift the reading of pins to before reading config so that even if ATTiny removes the signal, ESP can still take its own time in publishing the message
+    //Read the type of message we've got from the ATiny
+    #if (TESTING_MODE)
+      // Keep toggling the message type in testing mode
+      CURR_MSG = (CURR_MSG== SENSOR_OPEN?SENSOR_CLOSED:SENSOR_OPEN);
+    #else
+    if((digitalRead(SIGNAL_PIN0) == LOW) && (digitalRead(SIGNAL_PIN1) == LOW))
+      CURR_MSG = SENSOR_WAKEUP;
+    else if((digitalRead(SIGNAL_PIN0) == LOW) && (digitalRead(SIGNAL_PIN1) == HIGH))
+      CURR_MSG = SENSOR_OPEN;
+    else if((digitalRead(SIGNAL_PIN0) == HIGH) && (digitalRead(SIGNAL_PIN1) == LOW))
+      CURR_MSG = SENSOR_CLOSED;
+    //else nothing to do, invalid mode
+    #endif
 
-  #if (TESTING_MODE)
-  delay(5000);
-  #else
-  //Power down the ESP by writing LOW on HOLD PIN
-  //DPRINTLN("powering down");
-  digitalWrite(HOLD_PIN, LOW);  // set GPIO 0 low this takes CH_PD & powers down the ESP
-  #endif
+    // populate the values for the message
+    strcpy(myData.device_name,DEVICE_NAME);
+    myData.intvalue1 = (CURR_MSG == SENSOR_OPEN? MSG_ON:MSG_OFF);
+    myData.intvalue2 = ESP.getVcc();
+    myData.intvalue4 = 0;
+    myData.floatvalue1 = 0;
+    myData.floatvalue2 = 0;
+    myData.floatvalue3 = 0;
+    myData.floatvalue4 = 0;
+    myData.chardata1[0] = '\0';
+    strncpy(myData.chardata2,compile_version,15);//only copy the first 15 chars as compile_version is longer
+    myData.chardata2[15] = '\0';//add the null character else it will result in overflow of memory
+    //generate a random value for the message id
+    srand(time(0));
+    myData.message_id = rand();
+    myData.intvalue3 = millis();// for debug purpuses, send the millis till this instant in intvalue3
+    send_espnow_message(&myData);
+
+    #if (TESTING_MODE)// dont kill power to itself in testing mode, delay and keep continuing
+    delay(5000);
+    #else
+    //Power down the ESP by writing LOW on HOLD PIN
+    //DPRINTLN("powering down");
+    digitalWrite(HOLD_PIN, LOW);  // set GPIO 0 low this takes CH_PD & powers down the ESP
+    #endif
+  }
 
 }
 
